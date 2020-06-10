fa3ea8da7a9604622561d533537f6ee2809055ff
Marcus Gonçalves <marcus-goncalves@outlook.com.br>
2020-06-10 06:47:10 -0300

    projeto finalizado

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+//os recursos do miniframework
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class IndexController extends Action {
+
+	public function index() {
+		$this->view->login = isset($_GET['login']) ? $_GET['login'] : '';
+		$this->render('index');
+	}
+
+	public function inscreverse() {
+		$this->view->usuario = array(
+			'nome' => '',
+			'senha' => '',
+			'email' => ''
+		);
+		$this->view->erroCadastro = false;
+		$this->render('inscreverse');
+	}
+	public function registrar() {
+		# getModel faz a conexão com o db
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('nome',$_POST['nome']);
+		$usuario->__set('email',$_POST['email']);
+		$usuario->__set('senha',md5($_POST['senha']));
+
+		if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0){
+			$usuario->salvar();
+			$this->render('cadastro');
+		} else {
+			$this->view->usuario = array(
+				'nome' => $_POST['nome'],
+				'senha' => $_POST['senha'],
+				'email' => $_POST['email']
+			);
+			$this->view->erroCadastro = true;
+			$this->render('inscreverse');
+		}
+	}
+}
+
+
+?>
